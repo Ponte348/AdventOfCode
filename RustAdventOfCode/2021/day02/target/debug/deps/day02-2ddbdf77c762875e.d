005301f44889b436b5ffaf86e6f62a4fa1178aec
Ponte348 <ponte@ua.pt>
2024-02-19 09:49:23 +0000

    Day02 done

---
+++ RustAdventOfCode/2021/day02/target/debug/deps/day02-2ddbdf77c762875e.d
@@ -0,0 +1,5 @@
+/home/ponte/AdventOfCode/RustAdventOfCode/2021/day02/target/debug/deps/libday02-2ddbdf77c762875e.rmeta: src/main.rs
+
+/home/ponte/AdventOfCode/RustAdventOfCode/2021/day02/target/debug/deps/day02-2ddbdf77c762875e.d: src/main.rs
+
+src/main.rs:
---
+++ RustAdventOfCode/2021/day02/target/debug/deps/day02-3d84bff32b7e5585.d
@@ -0,0 +1,5 @@
+/home/ponte/AdventOfCode/RustAdventOfCode/2021/day02/target/debug/deps/libday02-3d84bff32b7e5585.rmeta: src/main.rs
+
+/home/ponte/AdventOfCode/RustAdventOfCode/2021/day02/target/debug/deps/day02-3d84bff32b7e5585.d: src/main.rs
+
+src/main.rs:
